a6172643dafd89e0c8539af68d2bfdc8515542a7
jy604 <jeongyoungp@gmail.com>
2024-07-28 13:57:22 +0900

    Add: model

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "jy");
         return "greetings"; // greetings.mustache 파일 반환
     }
 }
