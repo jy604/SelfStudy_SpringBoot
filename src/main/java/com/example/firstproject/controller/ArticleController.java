e826a4c7f85a2e1a9d86a3e2e388cea16d2059ee
jy604 <jeongyoungp@gmail.com>
2024-07-30 10:05:50 +0900

    Add:form data DTO로 ã받아오기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,7 +1,9 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -9,4 +11,12 @@ public class ArticleController {
     public String newArticleForm() {
         return "articles/new";
     }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+        return "";
+    }
 }
+
+
