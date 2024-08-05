61b7119b04c33cff80c30c086af8d795c0df1b30
jy604 <jeongyoungp@gmail.com>
2024-08-05 23:03:44 +0900

    Add: 페이지 연결 및 리다이렉트

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -12,6 +13,7 @@ import lombok.ToString;
 @NoArgsConstructor // 매개변수 없는 기본생성자 추가
 @ToString
 @Entity
+@Getter
 public class Article {
     @Id
     @GeneratedValue // 대표값 자동 생성
@@ -23,4 +25,8 @@ public class Article {
     @Column
     private String content;
 
+    // lombok 사용 안할 시
+//    public Long getId() {
+//        return id;
+//    }
 }
