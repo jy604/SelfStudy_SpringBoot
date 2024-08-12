129270ed0cf5016a8b6eba6a093cfad3146eac77
jy604 <jeongyoungp@gmail.com>
2024-08-12 21:40:17 +0900

    Feat: REST Api-GET

---
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,23 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleRepository articleRepository;
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+    // POST
+    // PATCH
+    // DELETE
+}
