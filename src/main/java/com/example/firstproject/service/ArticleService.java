f4a167533b84960309c4ef83fe87f3ab676a2afa
jy604 <jeongyoungp@gmail.com>
2024-08-14 18:35:42 +0900

    Add: 트랜잭션 실습

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -6,8 +6,10 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service // 서비스 객체 생성
@@ -61,5 +63,22 @@ public class ArticleService {
         // 3. 대상 삭제
         articleRepository.delete(target);
         return target;
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // 1. dto 묶음을 엔티티 묶음으로 변환
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+        // 2. 엔티티 묶음을 DB에 저장
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+        // 3. 강제 예외 발생시키기
+        // IllegalArgumentException는 전달값이 없거나 유효하지 않은 경우임
+        articleRepository.findById(-1L)
+                .orElseThrow(() -> new IllegalArgumentException("결제 실패!"));
+        // 4. 결과값 반환하기
+        return articleList;
     }
 }
