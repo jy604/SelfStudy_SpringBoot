c9b9a0587ed824a7d26d25a5e6830a92b4e3ead8
jy604 <jeongyoungp@gmail.com>
2024-08-14 16:26:07 +0900

    생성자 에러 주석 추가

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -6,9 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+// @allargsConstructor 사용하니 기본 생성자 없다고 오류 발생
+// @all 지우고 @no 와 @setter 붙이니 오류 풀림
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class ArticleForm {
     private Long id;
